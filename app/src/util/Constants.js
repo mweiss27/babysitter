b4f6044e29ccfec0f19fff97649fdc0b6df28aec
mweiss <mweiss@pretalen.com>
2018-06-18 09:11:48 -0400

    Fix error message

--- app/src/util/Constants.js
+++ app/src/util/Constants.js
@@ -1,7 +1,7 @@
 export default {
     CANNOT_END_BEFORE_START: "The babysitter cannot end before they start",
     CANNOT_START_BEFORE_5: "The babysitter cannot start before 5:00PM",
-    CANNOT_START_AFTER_359: "The babysitter cannot start before 3:59AM",
+    CANNOT_START_AFTER_359: "The babysitter cannot start after 3:59AM",
 
     CANNOT_END_BEFORE_501: "The babysitter cannot end before 5:01PM",
     CANNOT_END_AFTER_4: "The babysitter cannot stay after 4:00AM"
